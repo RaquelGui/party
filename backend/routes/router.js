a8f3c8fd89a3e8127d0c15a67a2b1720c68c8e7e
unknown <raquelgoulart@gmail.com>
2024-10-30 20:24:56 +0100

    update backend

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -6,4 +6,9 @@ const servicesRouter = require("./services");
 
 router.use("/", servicesRouter);
 
+//Parties router
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
+
 module.exports = router;
